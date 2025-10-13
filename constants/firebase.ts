761e1061c48084e152be1630a6e91a7624cc2ea7
千葉幸哉 <c4p31072@bunkyo.ac.jp>
2025-10-14 02:55:38 +0900

    Create firebase.ts

---
+++ constants/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHiMCeiOFN-R4EoLTiBpeJWIUQYIrUA5M",
+  authDomain: "beacon-haptic-sim.firebaseapp.com",
+  projectId: "beacon-haptic-sim",
+  storageBucket: "beacon-haptic-sim.appspot.com",
+  messagingSenderId: "699174750938",
+  appId: "1:699174750938:web:1ade40d8ef1a30c9c0b3e3",
+  measurementId: "G-4QX9T79SL7"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
