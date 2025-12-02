fe40e8a6efb0f81211ae349331ab8745136df62b
yamadaeita <c4p31116@bunkyo>
2025-12-03 00:09:12 +0900

    変更

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jp.teamd.beaconapp',
+  appName: 'beacon-haptic-sim',
+  webDir: 'docs'
+};
+
+export default config;
